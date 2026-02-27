73c7fdf5e3ae1a9ee6f8948485b0ccd7abfd1990
DanielDM123 <94938720+DanielDM123@users.noreply.github.com>
2026-02-27 23:56:48 +0000

    done

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
-import { createClient } from '@supabase/supabase-js'
+// This is how you import the supabase
+import { createClient } from 'https://cdn.skypack.dev/@supabase/supabase-js' 
 const supabaseUrl = 'https://mykcltgkxycmmcricdek.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im15a2NsdGdreHljbW1jcmljZGVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4NzQ0ODgsImV4cCI6MjA4NzQ1MDQ4OH0.ySdqe0r0XTkm6Src7AfQK2aryk7M3mEAyBLM9Lmod78'
 const supabase = createClient(supabaseUrl, supabaseKey)
@@ -8,7 +9,8 @@ let { data: books, error } = await supabase
   .from('books')
   .select('*')
 
-  for (let book of books) {
-    let bookllist = document.getElementById('books');
-    bookllist.innerHTML += `<li>&{book.title}</li>`
-  }
+for (let book of books) {
+  let bookllist = document.getElementById('books');
+  bookllist.innerHTML += `<th>${book.title}</th><th>${book.author}</th><th>${book.isbn}</th>`
+}
+
